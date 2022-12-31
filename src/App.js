a1b8e17aa91e7ad7335d86270b574d82d8d8f90e
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-01-01 01:58:58 +0530

    modified:   src/App.css 	modified:   src/App.js 	new file:   src/assets/ai.png 	new file:   src/assets/atlassian.png 	new file:   src/assets/blog01.png 	new file:   src/assets/blog02.png 	new file:   src/assets/blog03.png 	new file:   src/assets/blog04.png 	new file:   src/assets/blog05.png 	new file:   src/assets/dropbox.png 	new file:   src/assets/google.png 	new file:   src/assets/logo.png 	new file:   src/assets/logo.svg 	new file:   src/assets/people.png 	new file:   src/assets/possibility.png 	new file:   src/assets/shopify.png 	new file:   src/assets/slack.png 	deleted:    src/components/index .js 	new file:   src/components/index.js 	modified:   src/containers/index.js 	new file:   src/index.css 	modified:   src/index.js 	new file:   src/tempCodeRunnerFile.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { Footer,Blog,Possibility,Features,WhatGPT3,Header } from './containers';
-import { CTA,Brand,Navbar} from './components';
+import { CTA,Brand,Navbar} from './components' ;
+import './App.css'
 const App = () => {
   return (
     <div className = 'App'>
